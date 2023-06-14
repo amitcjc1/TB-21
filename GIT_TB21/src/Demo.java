e990257d88bc8c1d3320dd75e163968564e6f394
amit <amitcjcpune@gmail.com>
2023-06-14 16:24:15 +0530

    By Abc

--- GIT_TB21/src/Demo.java
+++ GIT_TB21/src/Demo.java
@@ -1,6 +1,14 @@
 
 public class Demo 
 {
+	
+	public void m1()
+	{
+		System.out.println("Hello This is M1");
+	}
+	
+	
+	
 public static void main(String[] args) {
 	System.out.println("Hello");
 }
